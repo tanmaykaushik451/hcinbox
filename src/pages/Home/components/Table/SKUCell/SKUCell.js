06cdba40f03ec52ed16bdbb0a2e240fc22b37e46
tanmaykaushik451 <Tanmay.kaushik451@gmail.com>
2021-09-02 19:36:28 +0530

    sku cell

--- src/pages/Home/components/Table/SKUCell/SKUCell.js
+++ src/pages/Home/components/Table/SKUCell/SKUCell.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import "./SKUCell.css"
 
 const SKUCell = () => {
     return (
-        <div>
-            sku
-        </div>
+        <span className="badge badge-primary statuscell bg-light text-dark mr-1 skucell">9177</span>
     )
 }
 
