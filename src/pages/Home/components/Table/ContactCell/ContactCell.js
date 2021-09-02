94e95014cff7e05bee111acd30a68ef4e365b1ed
tanmaykaushik451 <Tanmay.kaushik451@gmail.com>
2021-09-02 19:19:55 +0530

    contact cell

--- src/pages/Home/components/Table/ContactCell/ContactCell.js
+++ src/pages/Home/components/Table/ContactCell/ContactCell.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import sample from "../../../../../assets/sample.png"
+import "./ContactCell.css"
 
 const ContactCell = () => {
     return (
-        <div>
-            contact
+        <div className="d-flex justify-content-between">
+        <div className="d-flex align-items-center">
+            <div className="contactcell-img mr-2">
+                <img src={sample}/>
+            </div>
+            <div className="d-flex flex-column">
+                <span>Anna White</span>
+                <small>Hi can you help me with...</small>
+            </div>
+        </div>
+        <small className="text-lightgrey">5s ago</small>
         </div>
     )
 }
