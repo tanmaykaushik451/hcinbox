bfe20f09500cce82d6ddfbbcedfd4fae4536c016
tanmaykaushik451 <Tanmay.kaushik451@gmail.com>
2021-09-02 17:15:14 +0530

    navbar

--- src/layout/Navigation/Navigation.js
+++ src/layout/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Navigation.css"
 
 const Navigation = (props) => {
 
@@ -6,13 +7,30 @@ const Navigation = (props) => {
 
 
     return (
-
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#" onClick={() => toggleMenu()}>Menu</a>
+        <>
+            <a className="navbar-brand menubtn" href="#" onClick={() => toggleMenu()}><i class="fas fa-bars"></i></a>
+        <nav className="navbar navbar-expand-lg">
+            <div className="d-flex flex-column w-100">
+                <div className="d-flex justify-content-md-between justify-content-sm-center align-items-center">
+                    <nav aria-label="breadcrumb">
+                        <ol className="breadcrumb bg-white">
+                            <li className="breadcrumb-item" aria-current="page"><a className="text-dark" href="#">All Leads</a></li>
+                            <li className="breadcrumb-item" aria-current="page"><a className="text-dark" href="#">By Category</a></li>
+                            <li className="breadcrumb-item" aria-current="page">Wallpapers</li>
+                        </ol>
+                    </nav>
+                    <div>
+                        <button className="btn btn-primary btn-sm mr-2">Save View</button>
+                        <button className="btn btn-light btn-sm"><i className="fas fa-ellipsis-h"></i></button>
+                    </div>
+                </div>
             <div>
-                hello
+                <h5 className="ml-3">Wallpapers</h5>
             </div>
+            </div>
+
         </nav>
+        </>
 
     )
 }
